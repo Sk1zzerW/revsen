54f5a3c3fcfe597036e28446b0f50bca73ef26c2
olegk <kbryzhenyukpc@gmail.com>
2024-11-15 23:28:10 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("ваше предложение");
+        string input = Console.ReadLine();
+
+        string[] words = input.Split(' ');
+
+        string result = "";
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            string reversedWord = "";
+            for (int j = words[i].Length - 1; j >= 0; j--)
+            {
+                reversedWord += words[i][j];
+            }
+            result += reversedWord + " ";
+        }
+
+        Console.WriteLine("результат -  " + result.Trim());
+
+        Console.ReadKey();
+    }
+}
